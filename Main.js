b3bffe9226af23442d6c0193437fa2e41f755fde
Nehalokesh <nehal19697@gmail.com>
2022-09-13 12:55:07 +0530

    Task3 Commit

---
+++ Main.js
@@ -0,0 +1,8 @@
+console.log(document.domain)
+ console.log(document.url)
+ console.log(document.title)
+ console.log(document.doctype)
+
+
+var headerTitle = document.getElementById('header-title');
+onsole.log(headerTitle);
