4297154a722a8c6da6c3ef8b0e434a140164ffc8
Nehalokesh <nehal19697@gmail.com>
2022-09-13 15:07:53 +0530

    Task6(2) Commit

--- Main.js
+++ Main.js
@@ -1,34 +1,34 @@
 // queryselector
-var header=document.querySelector('#main-header');
-header.style.borderBottom='solid 4px #ccc';
+//var header=document.querySelector('#main-header');
+//header.style.borderBottom='solid 4px #ccc';
 
-var input = document.querySelector('input');
-input.value='Hello world'
+//var input = document.querySelector('input');
+//input.value='Hello world'
 
-var input = document.querySelector('input[type="submit"]');
-input.value="SEND"
+//var input = document.querySelector('input[type="submit"]');
+//input.value="SEND"
 
-var item =document.querySelector('.list-group-item');
-item.style.color='red';
+//var item =document.querySelector('.list-group-item');
+//item.style.color='red';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color ='blue';
+//var lastItem = document.querySelector('.list-group-item:last-child');
+//lastItem.style.color ='blue';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color ='green';
+//var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color ='green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.color ='white';
+//var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+//thirdItem.style.color ='white';
 
 //Queryselector all
-//var titles=document.querySelectorAll('.title')
-//console.log(titles);
-//titles[0],textcontent='Hello';
+var titles=document.querySelectorAll('.title')
+console.log(titles);
+titles[0],textcontent='Hello';
 
-//var odd=document.querySelectorAll('li:nth-child(odd');
-//var even=document.querySelectorAll('li:nth-child(even')
-//for (var i=0; i<odd.length; i++){
-    //odd[i].style.backgroundColor = 'green'
-    //even[i].style.backgroundColor = '#f4f4f4';
-//}
+var odd=document.querySelectorAll('li:nth-child(odd');
+var even=document.querySelectorAll('li:nth-child(even')
+for (var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green'
+    even[i].style.backgroundColor = '#f4f4f4';
+}
 
