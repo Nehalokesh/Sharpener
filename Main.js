ed5e25ac4edd9a81290d1302a450f1958a4c7555
Nehalokesh <nehal19697@gmail.com>
2022-09-13 14:16:00 +0530

    Task5 Commit

--- Main.js
+++ Main.js
@@ -1,5 +1,11 @@
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[3]);
-items[2].style.fontWeight ='bold';
-items[2].style.backgroundColor ='green';
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+//li[1].style.backgroundColor = 'yellow';
+
+for(var i = 0; i < items.length; i++){
+    li[i].style.backgroundColor = 'af4f4f4';
+}
+
