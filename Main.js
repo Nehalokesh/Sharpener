6513eb775ecb19a8fb2b646bc13738073fc29ce1
Nehalokesh <nehal19697@gmail.com>
2022-09-13 17:03:43 +0530

    Task7 Commit

--- Main.js
+++ Main.js
@@ -1,34 +1,72 @@
-// queryselector
-//var header=document.querySelector('#main-header');
-//header.style.borderBottom='solid 4px #ccc';
+// Traversing The Dom
+var itemList =document.querySelector('#items');
+//parentNode
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor ='#f4f4f4';
+//console.log(itemList.parentNode.parentNode.parentNode);
 
-//var input = document.querySelector('input');
-//input.value='Hello world'
+//parent Element
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor ='#f4f4f4';
+//console.log(itemList.parentElement.parentElement.parentElement);
 
-//var input = document.querySelector('input[type="submit"]');
-//input.value="SEND"
+// child Nodes
+console.log(itemList.childNodes);
 
-//var item =document.querySelector('.list-group-item');
-//item.style.color='red';
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor ='yellow';
 
-//var lastItem = document.querySelector('.list-group-item:last-child');
-//lastItem.style.color ='blue';
+// First Child
+console.log(itemList.firstChild);
+// // FirstElementchild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent ='Hello 1';
 
-//var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-//secondItem.style.color ='green';
+// last child
+console.log(itemList.lastChild);
+// lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent ='Hello 4';
 
-//var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-//thirdItem.style.color ='white';
+//next sibling
+console.log(itemList.nextSibling);
 
-//Queryselector all
-var titles=document.querySelectorAll('.title')
-console.log(titles);
-titles[0],textcontent='Hello';
+//next elementsibling
+console.log(itemList.nextElementSibling);
 
-var odd=document.querySelectorAll('li:nth-child(odd');
-var even=document.querySelectorAll('li:nth-child(even')
-for (var i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'green'
-    even[i].style.backgroundColor = '#f4f4f4';
-}
+//previousSibling
+console.log(itemList.previousSibling);
 
+//previouselementsibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color ='green';
+
+// Create Element
+//Create div
+var newDiv = document.createElement('div');
+
+// ADD class
+newDiv.className ='hello';
+
+// add id
+newDiv.id='hello1';
+
+
+// add attr
+newDiv.setAttribute('title', 'hello div');
+
+// create text node
+var newDivText = document.createTextNode('Hello');
+
+// ADD text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize= '30px';
+
+container.insertBefore(newDiv, h1);
