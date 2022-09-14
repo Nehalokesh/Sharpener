3f3b84c8ba68ab1f923d2ad7c5d96b5260b4cee4
Nehalokesh <nehal19697@gmail.com>
2022-09-14 16:58:28 +0530

    Task12 Commit

--- Main.js
+++ Main.js
@@ -1,87 +1,28 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-// Form submit event
-form.addEventListener('submit', onSubmit);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+myform.addEventListener('submit',onSubmit);
 
-//Adding edit button to all list item
-for(let i=0; i<itemList.length; i++){
-    const editBtn=document.createElement('button');
-    editBtn.className='btn btn-sm float-right editBtn';
-    editBtn.appendChild(document.createTextNode('EDIT'))
-
-    itemList[i].appendChild(editBtn)
-}
-
-
-
-// Add item
-function onSubmit(e){
+function onSubmit(e) {
   e.preventDefault();
 
-  // Get input value
-  var inputValue = document.getElementById(`item`).value;
-  var description = document.getElementById(`description`).value;
-  // Create new li element
-  var li = document.createElement(`li`);
-  // Add class
-  li.className = `list-group-item`;
+  if(nameInput.value === '' || emailInput.value === ''){
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
 
-  const newText =document.createTextNode(inputValue)
-  const descriptionNode =document.createTextNode(description)
+    setTimeout(()=> msg.remove(), 3000);
+  
+  } else {
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
 
-  // Add text node with input value
-  li.appendChild(newText);
-  li.appendChild(descriptionNode);
-
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
+    userList.appendChild(li);
+    // clear fields
+    nameInput.value ='';
+    emailInput.value='';
   }
 }
 
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    var description = item.childNodes[1].textContent;
-
-    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
- 
-}
